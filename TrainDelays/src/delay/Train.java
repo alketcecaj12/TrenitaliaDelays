9cf1feb32ca946818af51124d2392ed1f32c5378
Alket Cecaj <alket.cecaj>
2017-03-21 17:18:55 +0100

    first commit

---
+++ TrainDelays/src/delay/Train.java
@@ -0,0 +1,27 @@
+package delay;
+
+public class Train {
+	
+	public String stazione ; 
+	public Long orario;
+	public Long orarioEff;
+    public double delay;
+
+
+	public Train(String s, long o, long oe, double d){
+		
+		this.stazione = s; 
+		
+		this.orario = o; 
+		
+		this.orarioEff = oe; 
+		this.delay = d;
+		
+	}
+	
+	public String toString(){
+		return stazione+","+orario+","+orarioEff+","+delay;
+	}
+	
+	
+}
